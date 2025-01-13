2b2fca70673f3151f86896c27b70d51dbd987745
ju4n-avendanoa <juanfernando.avendanoa@gmail.com>
2025-01-13 16:09:55 -0500

    codigo de clientes agregado

--- clientes.sql
+++ clientes.sql
@@ -1,26 +1,22 @@
-SELECT
-    T0."CardCode",
-    T0."CardName",
+SELECT DISTINCT
+    T0."CardCode" AS "NIT",
+    T0."CardName" AS "Cliente",
+    T0."CreateDate",
     T0."Address",
+    T0."City",
+    T3."Name" AS "Municipio",
     T0."Phone1",
     T0."Phone2",
-    T0."CntctPrsn",
-    T0."Balance",
-    T0."CreditLine",
-    T0."DebtLine",
-    T0."SlpCode",
     T0."Cellular",
     T0."E_Mail",
-    T0."County",
-    T0."CreateDate",
-    T0."validFrom",
-    T0."State",
-    T0."Password",
-    T0."U_BPCO_City",
+    T2."SlpName" AS "Asesor",
     T0."U_vendedor1",
-    T0."U_Vendedor2",
-    T0."U_Vendedor3",
-    T0."U_Vendedor4",
-    T0."U_BPCO_CS"
+    T0."U_Vendedor2"
 FROM
     OCRD T0
+    INNER JOIN CRD1 T1 ON T0."CardCode" = T1."CardCode"
+    INNER JOIN OSLP T2 ON T0."SlpCode" = T2."SlpCode"
+    LEFT JOIN "BD_PARTEQUIPOS_PRO"."@BPCO_MU" T3 ON T3."Code" = T0."U_BPCO_CS"
+WHERE
+    T0."validFor" = 'Y'
+    AND T0."CardType" = 'C'
--- ordenes_venta_abiertas.sql
+++ ordenes_venta_abiertas.sql
@@ -1,17 +1,17 @@
 SELECT
     T0."DocNum",
-    T2."SlpName",
+    T2."SlpName" AS "Asesor",
     T0."DocDate",
-    T0."CardName",
-    T1."ItemCode",
+    T0."CardName" AS "Cliente",
+    T1."ItemCode" AS "Referencia",
     T1."Quantity",
     T1."OpenQty",
     T1."LineStatus",
-    T1."LineTotal"
+    T1."LineTotal" AS "Total"
 FROM
     ORDR T0
     INNER JOIN RDR1 T1 ON T0."DocEntry" = T1."DocEntry"
     INNER JOIN OSLP T2 ON T0."SlpCode" = T2."SlpCode"
 WHERE
     T0."DocStatus" = 'O'
-    and T1."LineStatus" = 'O'
+    AND T1."LineStatus" = 'O'
